6808024b0cf5f568d0df33db7566e68e15e1654e
Santosh1234810 <santosh.vairale19@gmail.com>
2023-06-01 00:32:02 +0530

    first commi

---
+++ src/app/material-component/material.routing.ts
@@ -0,0 +1,52 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ManageCategoryComponent } from './manage-category/manage-category.component';
+import { RouteGaurdService } from '../Services/route-gaurd.service';
+import { ManageProductComponent } from './manage-product/manage-product.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
+import { ViewBillComponent } from './view-bill/view-bill.component';
+import { ManageUserComponent } from './manage-user/manage-user.component';
+
+
+export const MaterialRoutes: Routes = [
+    {
+        path:'category',
+        component:ManageCategoryComponent,
+        canActivate:[RouteGaurdService],
+        data: {
+            expectedRole: ['admin']
+        }
+    },
+    {
+        path:'product',
+        component:ManageProductComponent,
+        canActivate:[RouteGaurdService],
+        data: {
+            expectedRole: ['admin']
+        }
+    },
+    {
+        path:'order',
+        component:ManageOrderComponent,
+        canActivate:[RouteGaurdService],
+        data: {
+            expectedRole: ['admin','user']
+        }
+    },
+    {
+        path:'bill',
+        component:ViewBillComponent,
+        canActivate:[RouteGaurdService],
+        data: {
+            expectedRole: ['admin','user']
+        }
+    },
+    {
+        path:'user',
+        component:ManageUserComponent,
+        canActivate:[RouteGaurdService],
+        data: {
+            expectedRole: ['admin']
+        }
+    },
+];
